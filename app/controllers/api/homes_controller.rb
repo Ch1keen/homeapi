54f74c5fe2a881bc77adf91e29b3d1a8e09d0fb4
Ch1keen <gihoong7@gmail.com>
2022-05-11 11:21:57 +0900

    feat: Home#show, Home#create

--- app/controllers/api/homes_controller.rb
+++ app/controllers/api/homes_controller.rb
@@ -3,10 +3,34 @@
 module Api
   class HomesController < ApplicationController
     def index
+      @homes = Home.all
       render 'index'
     end
 
     def create
+      Home.create(home_params)
+      render json: {msg: true}
+    end
+
+    def show
+      @statuses = Home.find(params[:id]).statuses
+      @humidity = @statuses.map(&:humidity).last
+      @temperature = @statuses.map(&:temperature).last
+      @pm1p0 = @statuses.map(&:pm1p0).last
+      @pm2p5 = @statuses.map(&:pm2p5).last
+      @pm10 = @statuses.map(&:pm10).last
+      @gas = @statuses.map(&:gas).last
+      @brightness = @statuses.map(&:brightness).last
+      @fan = @statuses.map(&:fan).last
+      @window = @statuses.map(&:window).last
+
+      render 'show'
+    end
+
+    private
+
+    def home_params
+      params.require(:home).permit(:serial_number)
     end
   end
 end
